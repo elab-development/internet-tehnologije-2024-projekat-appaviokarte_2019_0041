ea836fa841483120c530057c2a0313e6d3f681ae
mionavukcevic <miona.m.vukcevic@gmail.com>
2025-10-16 19:07:38 +0200

    rutiranje

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <RegisterPage></RegisterPage>
-      <LoginPage></LoginPage>
-     <Home></Home>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Home */}
+        <Route path="/" element={<Home />} />
+
+        {/* Auth */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Fallback: nepoznata ruta ide na /login (po želji promeni u /) */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
